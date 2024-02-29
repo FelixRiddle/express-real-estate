63b36241fadbfd7b969019faf97d7f04b9d44bd5
Felix <fedecraft3312@gmail.com>
2024-02-29 12:06:04 -0300

    Get all test

--- src/server/routes/user/property/operation/get_all.spec.js
+++ src/server/routes/user/property/operation/get_all.spec.js
@@ -1,74 +1,67 @@
-// import dotenv from "dotenv";
+const { v4: uuidv4 } = require("uuid");
+const dotenv = require("dotenv");
 
-// import { serverUrl } from "../../../../src/controllers/env/env.js";
-// import { AuthAPI, envServerUrl } from "express-authentication";
-// import PropertyAPI from "../../../../src/api/user/property/PropertyAPI.js";
+const { AuthAPI, UserAPI } = require("express-authentication");
 
-// describe("Get all: ", () => {
-//     // Setup dotenv
-//     dotenv.config({
-//         path: ".env"
-//     });
+const { AUTHENTICATION, REAL_ESTATE } = require("../../../../../mappings/env/SERVER_URL_MAPPINGS");
+const PropertyAPI = require("../../../../../api/property/PropertyAPI");
+const createAxiosInstance = require("../../../../../public/js/axios/createAxiosInstance");
+
+test("Delete property", async () => {
+    // Setup dotenv
+    dotenv.config({
+        path: ".env"
+    });
     
-//     // Create user data
-//     const userData = {
-//         name: "Get all",
-//         email: "get_all_tests@email.com",
-//         password: "asd12345",
-//         confirmPassword: "asd12345"
-//     };
+    // Create user data
+    const email = `alistar_${uuidv4()}@email.com`;
+    const userData = {
+        name: "Alistar",
+        email: email,
+        password: "asd12345",
+        confirmPassword: "asd12345"
+    };
     
-//     const url = serverUrl();
-//     const api = new AuthAPI(userData, url);
+    // Auth api
+    const api = new AuthAPI(userData, AUTHENTICATION);
+    await api.registerUser();
+    await api.confirmUserEmailWithPrivateKey(userData.email);
+    const loginRes = await api.loginGetJwt();
     
-//     // Run asynchronous work before the tests start
-//     beforeEach(async function() {
-//         // Register, and login
-//         await api.createLoginGetInstance();
-//     });
+    // Create some property
+    const property = {
+        title: "Luxury house",
+        description: "This is a luxury house",
+        rooms: 3,
+        parking: 2,
+        bathrooms: 3,
+        street: 'Norris Road 1223',
+        latitude: 35.0831751,
+        longitude: -90.022207,
+        priceId: 5,
+        categoryId: 4,
+        image: "",
+        // This is here but in the endpoint it does nothing
+        published: true,
+        userId: this.userId,
+    };
     
-//     afterEach(async function() {
-//         await api.deleteUser();
-//     });
+    // Property api
+    const realEstateAxios = createAxiosInstance(REAL_ESTATE, "", loginRes.token);
+    const propertyApi = new PropertyAPI(realEstateAxios);
+    // We need to check that the property was successfully created
+    // Otherwise the test is useless
+    const createRes = await propertyApi.createProperty(property);
+    const propCreated = createRes.propertyCreated;
     
-//     it('Get all test', async function() {
-//         // The api is at 'auth2'
-//         const url = `${envServerUrl()}/auth2`;
-        
-//         const userPassword = "demacia_123";
-//         const userData = {
-//             name: "Garen",
-//             email: `garen_${uuidv4()}@demacia.com`,
-//             password: userPassword,
-//             confirmPassword: userPassword
-//         };
-        
-//         const propertyApi = new PropertyAPI(api.instance);
-        
-//         // Create some property
-//         const property = {
-//             title: "Luxury house",
-//             description: "This is a luxury house",
-//             rooms: 3,
-//             parking: 2,
-//             bathrooms: 3,
-//             street: 'Norris Road 1223',
-//             latitude: 35.0831751,
-//             longitude: -90.022207,
-//             priceId: 5,
-//             categoryId: 4,
-//             image: "",
-//             // This is here but in the endpoint it does nothing
-//             published: true,
-//             userId: this.userId,
-//         };
-//         await propertyApi.createProperty(property);
-        
-//         const properties = await propertyApi.getAll();
-        
-//         // Now delete every user property
-//         await propertyApi.deleteAll();
-        
-//         expect(properties.properties.length > 0).toBe(true);
-//     });
-// });
+    // Now delete every user property
+    const properties = await propertyApi.getAll();
+    const nonZeroProperties = properties.properties.length >= 0;
+    await propertyApi.userDeleteAll();
+    
+    // Delete user
+    const userApi = UserAPI.fromAuthenticatedAPI(api);
+    await userApi.delete();
+    
+    expect(propCreated && nonZeroProperties).toBe(true);
+});
